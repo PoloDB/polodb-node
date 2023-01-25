7a72889756e5e2d48a9d69b230bc557ee8b83bac
Vincent Chan <okcdz@diverse.space>
2023-01-25 21:30:00 +0800

    feat: compile new js object

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,21 @@
-import path from 'path';
-import { EventEmitter } from 'events';
-import SharedState, { Config } from './sharedState';
-import Collection from './colleciton';
-import child_process from 'child_process';
-import { TransactionType } from './transactionType';
-import os from 'os';
-import MsgTy from './msgTy';
-import { encode } from './encoding';
+import path from "path";
+import { EventEmitter } from "events";
+import SharedState, { Config } from "./sharedState";
+import Collection from "./colleciton";
+import child_process from "child_process";
+import os from "os";
+import MsgTy from "./msgTy";
+import { encode } from "./encoding";
 
 const defaultConfig: Config = {
-  executablePath: os.platform() === 'win32' ? path.join(__dirname, '../bin/polodb.exe') : path.join(__dirname, '../bin/polodb'),
+  executablePath:
+    os.platform() === "win32"
+      ? path.join(__dirname, "../bin/polodb.exe")
+      : path.join(__dirname, "../bin/polodb"),
   log: false,
 };
 
 class PoloDbClient extends EventEmitter {
-
   private __state: SharedState;
   private __shuttingDown: boolean = false;
 
@@ -24,40 +25,39 @@ class PoloDbClient extends EventEmitter {
       ...config,
     };
 
-    const params: string[] = ['--version'];
+    const params: string[] = ["--version"];
 
     return new Promise((resolve, reject) => {
-      const process = child_process.spawn(
-        mergedConfig.executablePath,
-        params,
-        {
-          stdio: ['pipe']
-        }
-      );
+      const process = child_process.spawn(mergedConfig.executablePath, params, {
+        stdio: ["pipe"],
+      });
 
       let stdOutBuffer = Buffer.alloc(0);
 
-      process.stdout.on('data', (chunk: Buffer) => {
+      process.stdout.on("data", (chunk: Buffer) => {
         const newBuffer = Buffer.alloc(stdOutBuffer.length + chunk.length);
         stdOutBuffer.copy(newBuffer, 0);
         chunk.copy(newBuffer, stdOutBuffer.length);
         stdOutBuffer = newBuffer;
       });
 
-      process.on('error', (err: Error) => {
+      process.on("error", (err: Error) => {
         reject(err);
       });
 
-      process.stdout.on('close', () => {
+      process.stdout.on("close", () => {
         const decoder = new TextDecoder();
         let content = decoder.decode(stdOutBuffer);
-        content = content.replace('\n', '');
-        resolve(content)
+        content = content.replace("\n", "");
+        resolve(content);
       });
     });
   }
 
-  public static async createConnection(dbPath: string, config?: Partial<Config>): Promise<PoloDbClient> {
+  public static async createConnection(
+    dbPath: string,
+    config?: Partial<Config>
+  ): Promise<PoloDbClient> {
     const client = new PoloDbClient(dbPath, config);
 
     await client.start();
@@ -77,22 +77,16 @@ class PoloDbClient extends EventEmitter {
       if (this.__shuttingDown) {
         this.__state.kill();
       }
-      this.emit('error', err);
+      this.emit("error", err);
     });
   }
 
-  public startTransaction(ty?: TransactionType): Promise<void> {
-    if (typeof ty === 'undefined') {
-      ty = TransactionType.Auto;
-    }
-    const pack = encode(ty);
-    return this.__state.sendRequest(MsgTy.StartTransaction, pack);
-  }
-
-  public dropCollection(name: string): Promise<void> {
-    const pack = encode(name);
-    return this.__state.sendRequest(MsgTy.Drop, pack);
-  }
+  // public startTransaction(ty?: TransactionType): Promise<void> {
+  //   if (typeof ty === "undefined") {
+  //     ty = TransactionType.Auto;
+  //   }
+  //   return this.__state.sendRequest(MsgTy.StartTransaction, pack);
+  // }
 
   public commit(): Promise<void> {
     return this.writeEmptyBodyWithType(MsgTy.Commit);
@@ -128,7 +122,6 @@ class PoloDbClient extends EventEmitter {
   get config(): Config {
     return { ...this.__state.config };
   }
-
 }
 
 export default PoloDbClient;
--- src/encoding.ts
+++ src/encoding.ts
@@ -1,32 +1,9 @@
-import { decode as decodeMsgPack, encode as encodeMsgPack, ExtensionCodec } from '@msgpack/msgpack';
-import ObjectId from './objectId';
-
-const extensionCodec = new ExtensionCodec();
-extensionCodec.register({
-  type: 0x07,
-  encode: (obj: unknown): Uint8Array | null => {
-    if (obj instanceof ObjectId) {
-      const buffer = new ArrayBuffer(16);
-      const view = new Uint8Array(buffer);
-      view.set(obj.data, 0);
-      return view;
-    }
-    return null;
-  },
-  decode: (data: Uint8Array) => {
-    const bytes = data.subarray(0, 12);
-    return new ObjectId(bytes);
-  },
-});
+import { type Document, serialize, deserialize } from "bson";
 
 export function decode(buffer: Uint8Array) {
-  return decodeMsgPack(buffer, {
-    extensionCodec,
-  });
+  return deserialize(buffer);
 }
 
-export function encode(obj: unknown) {
-  return encodeMsgPack(obj, {
-    extensionCodec,
-  });
+export function encode(obj: Document) {
+  return serialize(obj);
 }
